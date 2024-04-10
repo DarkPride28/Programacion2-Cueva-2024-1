a4ab0de44a7b3ff716b28fd4e6a539010067e759
DarkPride28 <tecnosebas28@gmail.com>
2024-04-10 10:14:33 -0500

    Agregando Semana4 v1

---
+++ Semana4/FuncionesMemoria.h
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FuncionesMemoria.h
+ * Author: cueva
+ *
+ * Created on 11 de mayo de 2023, 11:13 AM
+ */
+
+#ifndef FUNCIONESMEMORIA_H
+#define FUNCIONESMEMORIA_H
+void cargarAlumnos(int *&, const char* );
+#endif /* FUNCIONESMEMORIA_H */
